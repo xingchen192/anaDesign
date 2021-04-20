a0ea3f5dd724d4fdc1efe27cde6022b684328403
chenxing10 <chenxing10@zbmac-c02f32hdm.360buyad.local>
2021-04-20 17:39:54 +0800

    简历Excel文件上传，未实现数据库

--- src/main/java/com/bjxxkjdx/job_analysis/controller/dataController.java
+++ src/main/java/com/bjxxkjdx/job_analysis/controller/dataController.java
@@ -66,7 +66,7 @@ public class dataController {
                 return "格式错误";
             } else {
                 jobsDataList.add(new jobsData(lo.get(0).toString(), lo.get(1).toString(), lo.get(2).toString(), lo.get(3).toString()));
-                dataMapper.insertData(lo.get(0).toString(),lo.get(1).toString(),lo.get(2).toString(),lo.get(3).toString());
+                dataMapper.insertData(lo.get(0).toString(), lo.get(1).toString(), lo.get(2).toString(), lo.get(3).toString());
             }
         }
 
@@ -84,18 +84,18 @@ public class dataController {
 //
 //        }
 
-        List<jobsData> jobsDataListRes = utilsGetWordCloudData.utilsHandle(jobsDataList,nameLists);
+        List<jobsData> jobsDataListRes = utilsGetWordCloudData.utilsHandle(jobsDataList, nameLists);
         List<dataReturn> res = utilsGetWordCloudData.utilsGetDataStruct(jobsDataListRes);
 
         for (dataReturn dataReturn : res) {
 //            if (dataMapper.getValue(dataReturn.getJobName(), dataReturn.getName())!=null)
             List<Integer> values = dataMapper.getValue(dataReturn.getJobName(), dataReturn.getName());
             if (values.size() > 0) {
-                dataMapper.deleteData(dataReturn.getJobName(), dataReturn.getName(),String.valueOf(values.get(0)));
-                dataMapper.insertDataStruct(dataReturn.getJobName(),dataReturn.getName(),String.valueOf(dataReturn.getValue() + values.get(0)));
+                dataMapper.deleteData(dataReturn.getJobName(), dataReturn.getName(), String.valueOf(values.get(0)));
+                dataMapper.insertDataStruct(dataReturn.getJobName(), dataReturn.getName(), String.valueOf(dataReturn.getValue() + values.get(0)));
 //                System.out.println(dataMapper.updateDataStruct(dataReturn.getJobName(),dataReturn.getName(),String.valueOf(dataReturn.getValue())));
             } else {
-                dataMapper.insertDataStruct(dataReturn.getJobName(),dataReturn.getName(),String.valueOf(dataReturn.getValue()));
+                dataMapper.insertDataStruct(dataReturn.getJobName(), dataReturn.getName(), String.valueOf(dataReturn.getValue()));
             }
 
         }
@@ -103,5 +103,91 @@ public class dataController {
         return "上传成功";
     }
 
+
+    /**
+     * 简历Excel数据上传
+     *
+     * @param file
+     * @return
+     * @throws Exception
+     */
+    @PostMapping(value = "/upload2")
+    @ResponseBody
+    public String uploadExcelResume(@RequestParam(value = "file") MultipartFile file) throws Exception {
+
+        if (file.isEmpty()) {
+            return "文件不能为空";
+        }
+
+
+        InputStream inputStream = file.getInputStream();
+        List<List<Object>> list = importService.getBankListByExcel(inputStream, file.getOriginalFilename());
+        inputStream.close();
+
+
+//        List<String> nameLists = dataMapper.getAllJobTypes();
+        List<jobsData> jobsDataList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            List<Object> lo = list.get(i);
+            System.out.println(lo);
+            //TODO 随意发挥
+//           if (lo.get(0).equals("0")&&lo.get(6).equals("1")){
+//                if (lo.get(1)==null)
+//           }
+            if (lo.size() > 5) {
+                for (Object c : lo) {
+                    if (c == null)
+                        c = "";
+//                System.out.println(c);
+                }
+            }else {
+                return "格式错误";
+            }
+
+
+//            System.out.println(lo.get(0));
+//            System.out.println(lo.get(1));
+//            System.out.println(lo.get(2));
+//            System.out.println(lo.get(3));
+//            if (lo.size() > 5) {
+//                return "格式错误";
+//            } else {
+//                jobsDataList.add(new jobsData(lo.get(0).toString(), lo.get(1).toString(), lo.get(2).toString(), lo.get(3).toString()));
+//                dataMapper.insertData(lo.get(0).toString(),lo.get(1).toString(),lo.get(2).toString(),lo.get(3).toString());
+//            }
+        }
+
+//        for (jobsData jobsData : jobsDataList) {
+//            boolean flag = true;
+//            for (String c : nameLists) {
+//                if (jobsData.getJobName().contains(c)) {
+//                    jobsData.setJobName(c);
+//                    flag = false;
+//                }
+//            }
+//            if (flag) {
+//                jobsDataList.remove(jobsData);
+//            }
+//
+//        }
+
+//        List<jobsData> jobsDataListRes = utilsGetWordCloudData.utilsHandle(jobsDataList,nameLists);
+//        List<dataReturn> res = utilsGetWordCloudData.utilsGetDataStruct(jobsDataListRes);
+//
+//        for (dataReturn dataReturn : res) {
+////            if (dataMapper.getValue(dataReturn.getJobName(), dataReturn.getName())!=null)
+//            List<Integer> values = dataMapper.getValue(dataReturn.getJobName(), dataReturn.getName());
+//            if (values.size() > 0) {
+//                dataMapper.deleteData(dataReturn.getJobName(), dataReturn.getName(),String.valueOf(values.get(0)));
+//                dataMapper.insertDataStruct(dataReturn.getJobName(),dataReturn.getName(),String.valueOf(dataReturn.getValue() + values.get(0)));
+////                System.out.println(dataMapper.updateDataStruct(dataReturn.getJobName(),dataReturn.getName(),String.valueOf(dataReturn.getValue())));
+//            } else {
+//                dataMapper.insertDataStruct(dataReturn.getJobName(),dataReturn.getName(),String.valueOf(dataReturn.getValue()));
+//            }
+//
+//        }
+
+        return "上传成功";
+    }
 
 }
